ae19eefdfd8111e1c26ec966b8830e9238d98849
Your Name <you@example.com>
2022-03-07 11:33:02 -0300

    Primeira Versão Finalizada | V - 0.0.1

---
+++ colunaTres.py
@@ -0,0 +1,26 @@
+import pyautogui
+import time
+
+
+# 678, 323
+
+# 672, 592 - Jessilane
+
+    #600, 708
+
+
+pyautogui.hotkey("alt", "tab")
+
+while True:
+
+    pyautogui.hotkey("f5")
+    time.sleep(3)
+    pyautogui.moveTo(672, 592)
+    pyautogui.click()
+    time.sleep(3)
+    pyautogui.moveTo(600, 673)
+    pyautogui.click()
+    time.sleep(3)
+    pyautogui.moveTo(683, 283)
+    pyautogui.click()
+    time.sleep(3)
