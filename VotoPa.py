ac0c0ae8cdeef536ca68e5669f8423ddc9e8b771
M. Messias <mmessiasdev@gmail.com>
2022-04-26 11:06:17 -0300

    Versão Totalmente altomatizada do Bot

---
+++ VotoPa.py
@@ -0,0 +1,30 @@
+import pyautogui
+import time
+
+# Deixe o Editor de código em primeira tela e o browser com a tela de votação do BBB aberta em segunda tela.
+
+
+
+
+# while True:
+#     pyautogui.displayMousePosition()
+
+pyautogui.hotkey("win", "s")
+pyautogui.write("Vivaldi")
+pyautogui.hotkey("enter")
+time.sleep(4)
+pyautogui.write("https://gshow.globo.com/realities/bbb/bbb22/votacao/vote-na-final-do-bbb-22-quem-voce-quer-que-venca-15b3ec29-4d06-4db3-8bd7-d4d4bd050057.ghtml")
+pyautogui.hotkey("enter")
+time.sleep(3)
+
+
+while True:
+
+    proxVotacao = pyautogui.locateAllOnScreen('votarNovamente.png')
+
+    time.sleep(2)
+    pyautogui.click('Pa.png')
+    time.sleep(1.5)
+    pyautogui.click('captha.png')
+    time.sleep(1)
+    pyautogui.hotkey("f5")
